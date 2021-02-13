3da48d012438b7763e2d548f1428389773fa2333
Mapril Junior <mapril_juniorl@ufrrj.br>
2021-02-13 12:23:27 -0300

    atualizando ContaBancaria.java

---
+++ ContaBancaria.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class ContaBancaria{
+
+	private int conta;
+	private float saldo;
+	private Pessoa cliente;
+
+	private void modificaSaldo(){
+		System.out.println("Digite o novo saldo do cliente:");
+		Scanner saldo = new Scanner(System.in); 
+	}
+}
