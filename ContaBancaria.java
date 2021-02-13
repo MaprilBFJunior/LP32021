2d83257834cf9e7615bfc8e0ad20c49c2deee5ab
Mapril Junior <mapril_juniorl@ufrrj.br>
2021-02-13 13:16:06 -0300

    atualizando(2) ContaBancaria.java

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -6,9 +6,29 @@ public class ContaBancaria{
 	private int conta;
 	private float saldo;
 	private Pessoa cliente;
+	
+	public ContaBancaria(int conta, float saldo, Pessoa cliente) {
+		this.conta = conta;
+		this.saldo = saldo;
+		this.cliente = cliente;
+	}
 
 	private void modificaSaldo(){
 		System.out.println("Digite o novo saldo do cliente:");
 		Scanner saldo = new Scanner(System.in); 
 	}
+
+	private String retornaSaldo(){
+		String retorno = Float.toString(this.saldo);
+		return retorno;
+	}
+
+	private void imprimeConta(){
+		System.out.printf("Conta: " + conta);
+		System.out.printf("Cliente: " + cliente.pegaNome());
+		System.out.printf("Saldo em conta: " + saldo);
+
+	}
+
+	
 }
